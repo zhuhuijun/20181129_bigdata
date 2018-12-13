43671134ed1102cb9e3f612404decdced85a8bba
ubuntu1204s100 <ubuntu@s100.(none)>
2018-12-13 07:47:14 -0800

    gsgs

--- test/com/zzbj/test/TestFlieCoherency.java
+++ test/com/zzbj/test/TestFlieCoherency.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package com.zzbj.test;
 
 import java.io.IOException;
@@ -46,3 +47,53 @@ public class TestFlieCoherency
 		System.out.println("over");
 	}
 }
+=======
+package com.zzbj.test;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 测试文件一致性
+ * @author zhuhuijun
+ *
+ */
+public class TestFlieCoherency
+{
+	private FileSystem fileSystem;
+
+	@Before
+	public void initConf()
+	{
+		Configuration conf = new Configuration();
+		try
+		{
+			fileSystem = FileSystem.get(conf);
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	/**
+	 * 写操作
+	 */
+	@Test
+	public void writeFile() throws Exception
+	{
+		// hdfs dfs -chmod 777 /user/ubuntu
+		Path f = new Path("hdfs://s100:8020/user/how4334.txt");
+		FSDataOutputStream fsDataOutputStream = fileSystem.create(f);
+		fsDataOutputStream.write("hello,world".getBytes());
+		fsDataOutputStream.hflush();
+		//fsDataOutputStream.hsync();
+		fsDataOutputStream.close();
+		System.out.println("over");
+	}
+}
+>>>>>>> ed7132ccdc99bbdcb45ea85dc8a75fafe51861b6
