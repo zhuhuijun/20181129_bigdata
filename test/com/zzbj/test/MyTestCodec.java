b9a7cafde9aadbbe4a9511df4b2ec90c4ab96a66
ubuntu1204s100 <zhuhjtcx@qq.com>
2018-12-14 07:15:19 -0800

    fs

--- test/com/zzbj/test/MyTestCodec.java
+++ test/com/zzbj/test/MyTestCodec.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.zzbj.test;
 
 import java.io.FileInputStream;
@@ -41,47 +40,3 @@ public class MyTestCodec
 	}
 
 }
-=======
-package com.zzbj.test;
-
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-
-import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.io.compress.CompressionOutputStream;
-import org.apache.hadoop.io.compress.DefaultCodec;
-import org.junit.Test;
-
-import sun.nio.ch.IOUtil;
-
-/**
- * 测试压缩与解压缩
- * 
- * @author zhuhuijun
- *
- */
-public class MyTestCodec
-{
-	@SuppressWarnings("resource")
-	@Test
-	public void testDeflate() throws Exception
-	{
-		
-		FileInputStream fin = new FileInputStream("d:\\dd.pdf");
-		
-		
-		//创建解码器
-		DefaultCodec defaultCodec = new DefaultCodec();
-		FileOutputStream fos = new FileOutputStream("d:\\codec\\dd.deflate");
-		//创建压缩流
-		CompressionOutputStream cos = defaultCodec.createOutputStream(fos);
-		//流的对copy
-		IOUtils.copyBytes(fin, cos, 1024);
-		cos.close();
-		fos.close();
-		fin.close();
-		System.err.println("over");
-	}
-
-}
->>>>>>> d3ae47af51fce0521960e12814db3229de56de70
--- test/com/zzbj/test/TestCheckSum.java
+++ test/com/zzbj/test/TestCheckSum.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.zzbj.test;
 
 import java.io.IOException;
@@ -31,37 +31,4 @@ public class TestCheckSum
 	}
 
 }
-=======
-package com.zzbj.test;
 
-import java.io.IOException;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.fs.permission.FsPermission;
-import org.junit.Test;
-
-public class TestCheckSum
-{
-
-	@Test
-	public void testLocalFileSystem() throws Exception
-	{
-		// 创建配置对象加载本地的coredefault.xml core-site.xml;
-		Configuration config = new Configuration();
-		config.set("fs.defaultFS", "file:///");
-		// 本地文件系统
-		FileSystem fs = FileSystem.get(config);
-		Path path = new Path("d:\\aaa\\hello.txt");
-		FSDataOutputStream fos = fs.create(path,true);
-		fos.write("hello".getBytes());
-		fos.close();
-		fs.close();
-		System.out.println("over");
-
-	}
-
-}
->>>>>>> d3ae47af51fce0521960e12814db3229de56de70
